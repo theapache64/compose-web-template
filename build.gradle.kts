068bb123b79eb13c9483b9e35d020c9d2922e84c
theapache64 <theapache64@gmail.com>
2021-10-16 18:26:11 +0530

    🐛 fix webpack issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Add compose gradle plugin
 plugins {
-    kotlin("multiplatform") version "1.5.10"
-    id("org.jetbrains.compose") version "0.0.0-web-dev-14"
+    kotlin("multiplatform") version "1.5.31"
+    id("org.jetbrains.compose") version "1.0.0-alpha4-build398"
 }
 group = "com.theapache64"
 version = "1.0.0-alpha01"
@@ -25,4 +25,8 @@ kotlin {
             }
         }
     }
+}
+// Workaround for https://youtrack.jetbrains.com/issue/KT-49124
+rootProject.extensions.configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
+    versions.webpackCli.version = "4.9.0"
 }
